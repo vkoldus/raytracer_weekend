35a872f92ee001822fd1cce46bdd4aa9486b0bca
Vaclav Koldus <vaclav.koldus@sony.com>
2024-08-15 19:32:20 +0200

    Add a bubble into the glass sphere

--- include/path_tracer/world.h
+++ include/path_tracer/world.h
@@ -20,14 +20,17 @@ World make_world()
     auto material_center = std::make_shared<Lambertian>(Color(0.1, 0.2, 0.5));
     // Air bubble in water
     // auto material_left = std::make_shared<Dielectric>(1 / 1.33);
-    // Air
-    auto material_left = std::make_shared<Dielectric>(1.33);
+    // Glass
+    auto material_left = std::make_shared<Dielectric>(1.5);
+    // Air in glass
+    auto material_bubble = std::make_shared<Dielectric>(1.00 / 1.50);
     auto material_right = std::make_shared<Metal>(Color(0.8, 0.6, 0.2), 0.1);
 
     return {
             std::make_shared<Sphere>(Point3(0, -100.5, -1.0), 100, material_ground),
             std::make_shared<Sphere>(Point3(0, 0, -1.2), 0.5, material_center),
             std::make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left),
+            std::make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.4, material_bubble),
             std::make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_right),
     };
 }
